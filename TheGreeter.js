9642ab6ad6c18cd26d23e4a95d11be6e360ccf08
Vicky Koblinski <vickykoblinski@gmail.com>
2018-01-03 11:56:23 -0500

    Documentation

--- TheGreeter.js
+++ TheGreeter.js
@@ -1,46 +1,77 @@
 (function (global, $) {
 
+  /**
+   * Creates a new TheGreeter object
+   * @constructor
+   * @param {string} fname First name
+   * @param {string} lname Last name
+   * @param {string} [lang="en"] Language
+   */
   const TheGreeter = function (fname, lname, lang) {
     return new TheGreeter.init(fname, lname, lang);
   }
 
+  // Used for validation
   const supportedLangs = ['en', 'jp'];
 
-  const defaultGreetings = {
-    en: 'Hello',
-    jp: 'Konnichiwa'
-  };
+  // Data store for all greetings
+  const greetings = {
+    defaultGreetings: {
+      en: 'Hello',
+      jp: 'Konnichiwa'
+    },
+    morningGreetings: {
+      en: 'Good morning',
+      jp: 'Ohayō'
+    },
+    logMessages: {
+      en: 'Logged in',
+      jp: 'Roguin shita'
+    }
+  }
 
-  const morningGreetings = {
-    en: 'Good morning',
-    jp: 'Ohayō'
-  };
-
-  const logMessages = {
-    en: 'Logged in',
-    jp: 'Roguin shita'
-  };
-
+  // Public methods
   TheGreeter.prototype = {
 
+    /**
+     * Person's Full Name
+     * @return {string}
+     */
     fullName: function () {
       return `${this.fname} ${this.lname}`;
     },
 
+    /**
+     * Checks if language is supported.
+     * Throws error if language is unsupported.
+     */
     validate: function () {
       if (supportedLangs.indexOf(this.lang) === -1) {
         throw "Invalid language";
       }
     },
 
+    /**
+     * Basic greeting
+     * @return {string}
+     */
     greeting: function () {
-      return `${defaultGreetings[this.lang]} ${this.fname}!`;
+      return `${greetings.defaultGreetings[this.lang]} ${this.fname}!`;
     },
 
+    /**
+     * Morning greeting
+     * @return {string}
+     */
     morningGreeting: function () {
-      return `${morningGreetings[this.lang]} ${this.fname}.`;
+      return `${greetings.morningGreetings[this.lang]} ${this.fname}.`;
     },
 
+    /**
+     * Gets approprate greeting
+     * @param {bool} isMorning sets if should be morning greeting
+     * @return {this} chainable
+     */
     greet: function (isMorning) {
       let msg = this.greeting();
 
@@ -57,20 +88,35 @@
       return this;
     },
 
+    /**
+     * Prints name and language to console
+     * @return {this} chainable
+     */
     log: function () {
       if (console) {
-        console.log(`${logMessages[this.lang]}: ${this.fullName()}`)
+        console.log(`${greetings.logMessages[this.lang]}: ${this.fullName()}`)
       }
 
       return this;
     },
 
+    /**
+     * Sets language on the fly
+     * @param {string} [lang="en"]
+     * @returns {this} chainable
+     */
     setLang: function (lang = 'en') {
       this.lang = lang;
       this.validate();
       return this;
     },
 
+    /**
+     * Prints greeting to HTML element
+     * @param {string} element jQuery selector
+     * @param {bool} [morning=false] for displaying morning greeting 
+     * @return {this} chainable
+     */
     $Greet: function (element, morning) {
       if (!$) {
         throw 'jQuery not found';
@@ -92,14 +138,17 @@
     }
   };
 
+  //Called by constuctor to create object
   TheGreeter.init = function (fname, lname, lang) {
     this.fname = fname || "Unknown";
     this.lname = lname || "Unknown";
     this.lang = lang || "en";
   }
 
+  //Points init to TheGreeter's prototype
   TheGreeter.init.prototype = TheGreeter.prototype;
 
+  //Exposes TheGreeter/G to the global context
   global.TheGreeter = global.G = TheGreeter;
 
 })(window, jQuery);
