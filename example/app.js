13dc5ffc20ce6603c76758754675b67feaddd11b
Vicky Koblinski <vickykoblinski@gmail.com>
2018-01-03 12:59:50 -0500

    comments

--- example/app.js
+++ example/app.js
@@ -1,6 +1,12 @@
-const g = G('Joe', 'Bowman');
-
+// Adds a listener to the login button
 $('#login').click(function () {
-  var lang = $('#lang').val();
+
+  //Creates a new TheGreeter object for Joe Bowman
+  const g = G('Joe', 'Bowman');
+
+  //Gets the language option from the selector
+  const lang = $('#lang').val();
+
+  //Sets TheGreeter language and passes the '#greet' selector to render Joe's greeting to the screen.
   g.setLang(lang).$Greet('#greet');
 })
